287462d1db2fc74709565fa6db40c413e737d138
Murph Grainger <murph.grainger@gmail.com>
2017-01-20 14:08:08 -0700

    operation result

--- app.js
+++ app.js
@@ -14,3 +14,11 @@ function alphabet_position(str) {
 }
 
 alphabet_position("The sunset sets at twelve o' clock 222.")
+
+
+// The function should take three arguments - operation(string/char), value1(number), value2(number).
+// The function should return result of numbers after applying the chosen operation.
+
+function basicOp(operation, value1, value2) {
+    return eval(`${value1} ${operation} ${value2}`);
+}
